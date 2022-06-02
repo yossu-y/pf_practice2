300b34fce46d7df78a960d75411f733c7e13716a
yossu-y <yangyuub@gmail.com>
2022-06-02 08:43:53 +0000

    [update]homes-top

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.active_job.queue_adapter = :inline
+  config.hosts << "86e94a66b32c4c43bec1595ec452074c.vfs.cloud9.us-east-1.amazonaws.com"
+  
 end
